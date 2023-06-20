ebb976530232fd95ddee928276693e853121485d
shiva reddy <shivareddy1287@gmail.com>
2023-06-20 11:22:47 +0530

    delete copy file

--- puppeteer - Copy/server/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getUsers, createUser } from "../controllers/user.js";
-
-const router = express.Router();
-
-router.get("/users", getUsers);
-router.post("/user", createUser);
-
-export default router;
