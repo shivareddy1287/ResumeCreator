f7d64e35825b3f21a9b15d7dd6095c4b59280470
shiva reddy <shivareddy1287@gmail.com>
2023-06-20 10:55:35 +0530

    build update

--- server/index.js
+++ server/index.js
@@ -2,18 +2,32 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import userRoutes from "./routes/user.js";
-
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(process.cwd() + "/client/build/"));
+// app.use(express.static(process.cwd() + "../client/build/"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(process.cwd() + "../client/build/index.html");
+// });
+
+// app.use(express.static(path.join(__dirname, "../client/build")));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(process.cwd() + "/client/build/index.html");
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
-
 // app.use("/", (req, res) => {
 //   res.send("welcome to azure project");
 // });
